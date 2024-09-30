0a89b0209abe3161be6e80b0845d017dbdd8ab24
Aayush Singh <113753179+Peeppop@users.noreply.github.com>
2024-09-30 21:32:49 +0530

    Update and rename Addition.c to Add_Sub_Mul_Div.c
    This code give you addition , subtraction , division and multiplication  of two integers .

---
+++ Add_Sub_Mul_Div.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+ { 
+    
+  int a,b,c,d,e,f;
+printf("Enter value of a : ");
+scanf("%d", &a);
+printf("Enter value of b:");
+scanf("%d",&b);
+c=a+b;
+printf("The Sum is %d\n",c);
+  
+d=a-b;
+printf("Subtraction : %d\n", d);
+
+e=a*b;
+printf("Multiplication: %d\n",e);
+
+f=a/b;
+printf("Division: %d\n",f);
+
+}
